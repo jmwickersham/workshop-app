da2b6db0bffb50811c82d94086fcdd3ef0391d5b
Jennifer Wickersham <jennifer.wickersham@riotgames.com>
2018-10-15 15:03:20 -0700

    add sample video data

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { videos } from '../sample-data';
+
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
+
+  videos = videos;
 
   constructor() { }
 
